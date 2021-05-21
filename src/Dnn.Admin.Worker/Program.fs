65c87b9efef6288d6766e96a0b503b10b38faa5c
Josh M <20140997+jcmrva@users.noreply.github.com>
2021-05-21 17:17:48 -0400

    added worker project

---
+++ src/Dnn.Admin.Worker/Program.fs
@@ -0,0 +1,20 @@
+namespace Dnn.Admin.Worker
+
+open System
+open System.Collections.Generic
+open System.Linq
+open System.Threading.Tasks
+open Microsoft.Extensions.DependencyInjection
+open Microsoft.Extensions.Hosting
+
+module Program =
+    let createHostBuilder args =
+        Host.CreateDefaultBuilder(args)
+            .ConfigureServices(fun hostContext services ->
+                services.AddHostedService<Worker>() |> ignore)
+
+    [<EntryPoint>]
+    let main args =
+        createHostBuilder(args).Build().Run()
+
+        0 // exit code
